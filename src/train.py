5922489737de14bb15a649fe5949ff9bb796ccd7
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2025-09-18 01:52:10 +0000

    compute cer

--- src/train.py
+++ src/train.py
@@ -90,15 +90,19 @@ def compute_metrics(pred, tokenizer):
     pred_ids = pred.predictions
     label_ids = pred.label_ids
     
+    # convert -100 (ignored) tokens into pad tokens for decoding
     label_ids[label_ids == -100] = tokenizer.pad_token_id
-    
+
     pred_str = tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
     label_str = tokenizer.batch_decode(label_ids, skip_special_tokens=True)
-    
-    wer = evaluate.load("wer")
-    wer_score = 100 * wer.compute(predictions=pred_str, references=label_str)
-    
-    return {"wer": wer_score}
+
+    wer_metric = evaluate.load("wer")
+    cer_metric = evaluate.load("cer")
+
+    wer_score = 100 * wer_metric.compute(predictions=pred_str, references=label_str)
+    cer_score = 100 * cer_metric.compute(predictions=pred_str, references=label_str)
+
+    return {"wer": wer_score, "cer": cer_score}
 
 def main():
     # Load processor and model
